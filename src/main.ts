1fd33904ee64e7d3b830a49bd92aa50ff2401710
LinKR00001 <linkr00001@gmail.com>
2026-03-04 18:53:29 +0800

    部署支持可安装的应用

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import '@/styles/theme.css'
 
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/Configurator/sw.js')
+      .catch(err => console.error('SW registration failed:', err));
+  });
+}
+
 createApp(App).mount('#app')
